4656fdd0871a03998272479a51fef088b58e19fc
kavya <56983083+kavyarachita@users.noreply.github.com>
2022-09-27 18:19:45 -0400

    transferred skill build code

---
+++ game/Player.gd
@@ -0,0 +1,59 @@
+extends KinematicBody2D
+
+var GRAVITY = 30
+const MAXFALLSPEED = 3000
+const MAXSPEED = 500
+const JUMPFORCE = 1000
+var SPEED = 20
+var usedflap = false
+
+var velocity = Vector2()
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _physics_process(delta):
+	get_input()	
+	velocity = move_and_slide(velocity, Vector2.UP)
+
+func get_input():
+	if velocity.y > MAXFALLSPEED:
+		velocity.y = MAXFALLSPEED
+	
+	velocity.x = clamp(velocity.x, -MAXSPEED, MAXSPEED)
+	
+	if Input.is_action_pressed("right"):
+		if velocity.x < -MAXSPEED:
+			velocity.x = MAXSPEED
+		else:
+			velocity.x += SPEED
+		$AnimatedSprite.play("Walk")
+		$AnimatedSprite.flip_h = false
+	elif Input.is_action_pressed("left"):
+		if velocity.x > MAXSPEED:
+			velocity.x = MAXSPEED
+		else:
+			velocity.x -= SPEED
+		$AnimatedSprite.flip_h = true
+		$AnimatedSprite.play("Walk")
+	else:
+		if Input.is_action_pressed("crouch"):
+			$AnimatedSprite.play("Crouch")
+		else:
+			$AnimatedSprite.play("Idle")
+		velocity.x = lerp(velocity.x, 0, 0.2)
+		
+	if is_on_floor():
+		usedflap = false
+		if Input.is_action_just_pressed("jump"):
+			velocity.y = -JUMPFORCE
+	else: 
+		if usedflap == false and Input.is_action_just_pressed("jump"):
+			velocity.y = -JUMPFORCE
+			usedflap = true
+		velocity.y += GRAVITY
+	
+
+func _on_Area2D_body_entered(body):
+	print("something hit")
+	if body.is_in_group("hurtbox"):
+		print("sometihin took damage")
+		body.take_damage(35)
---
+++ game/Rat.gd
@@ -0,0 +1,82 @@
+extends KinematicBody2D
+
+const SPEED = 10
+var GRAVITY = 15
+const MAXFALLSPEED = 600
+const MAXSPEED = 100
+
+var velocity = Vector2()
+
+var dirleft = false
+
+var loop = 0
+var rng = RandomNumberGenerator.new()
+var loopthres = 100
+var idle = false
+
+var health = 100
+var isdead = false
+var handlingdeath = false
+
+func take_damage(dmg):
+	print("damaged!!!")
+	health -= dmg
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _physics_process(delta):
+	if health <= 0:
+		isdead = true
+	if isdead and not handlingdeath:
+		handlingdeath = true
+		loop = 0
+		loopthres = 30
+		$AnimatedSprite.flip_v = true
+	elif isdead and handlingdeath:
+		loop += 1
+		if loop > loopthres:
+			self.queue_free()
+	else:
+		loop += 1
+		velocity = move_and_slide(velocity, Vector2.UP)
+		
+		if velocity.y > MAXFALLSPEED:
+			velocity.y = MAXFALLSPEED
+			
+		if not is_on_floor():
+			GRAVITY *= 1.009
+			velocity.y += GRAVITY
+		
+		if dirleft and not idle:
+			$AnimatedSprite.play("Walk")
+			$AnimatedSprite.flip_h = false
+			if velocity.x < -MAXSPEED:
+				velocity.x = -MAXSPEED
+			else:
+				velocity.x -= SPEED
+			if loop > loopthres:
+				print("Left?: ",dirleft,"   Idle?: ",idle,"   loop/thres: ",loop,"/",loopthres)
+				idle = true
+				loopthres = rng.randf_range(10,100)
+				dirleft = false
+				loop = 0
+		elif not dirleft and not idle:
+			$AnimatedSprite.play("Walk")
+			$AnimatedSprite.flip_h = true
+			if velocity.x > MAXSPEED:
+				velocity.x = MAXSPEED
+			else:
+				velocity.x += SPEED
+			if loop > loopthres:
+				print("Left?: ",dirleft,"   Idle?: ",idle,"   loop/thres: ",loop,"/",loopthres)
+				idle = true
+				loopthres = rng.randf_range(10,100)
+				loop = 0
+				dirleft = true
+		else:
+			$AnimatedSprite.play("Idle")
+			velocity.x = lerp(velocity.x, 0, 0.2)
+			if loop > loopthres:
+				print("Left?: ",dirleft,"   Idle?: ",idle,"   loop/thres: ",loop,"/",loopthres)
+				idle = false
+				loopthres = rng.randf_range(100,300)
+				loop = 0
