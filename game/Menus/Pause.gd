490051c6b6122d6e5511bc87d94ec2c0daf5380f
abbygalelim <akl9bya@virginia.edu>
2022-10-19 22:29:59 -0400

    Commenting unused Pause nodes out

--- game/Menus/Pause.gd
+++ game/Menus/Pause.gd
@@ -16,11 +16,11 @@ func _physics_process(delta):
 		"""
 		get_tree().paused = false
 		get_tree().change_scene("res://Menu.tscn")
-
+""""
 onready var selector_one = $CanvasLayer/Pause/CenterContainer/VBoxContainer/CenterContainer2/VBoxContainer/CenterContainer/HBoxContainer/Label
 onready var selector_two = $CanvasLayer/Pause/CenterContainer/VBoxContainer/CenterContainer4/VBoxContainer/CenterContainer/HBoxContainer/Label
 onready var selector_three = $CanvasLayer/Pause/CenterContainer/VBoxContainer/CenterContainer3/VBoxContainer/CenterContainer/HBoxContainer/Label
-"""
+
 var current_selection = 0
 
 # Called when the node enters the scene tree for the first time.
